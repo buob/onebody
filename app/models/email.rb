383ac0c75d31e1e2f12ba13696b1bf5eb37409f0
Justin Richter <jrichter@jetfive.com>
2014-07-30 23:19:42 -0500

    getting closer

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,4 +1,4 @@
-class Email < ActiveRecord::Base
+class Email
 
   include HTTParty
 
@@ -7,17 +7,34 @@ class Email < ActiveRecord::Base
   base_uri 'https://api.mailgun.net/v2'
   basic_auth 'api', APIKEY
 
-  def self.show_routes(skip: 1, limit: 1)
+  def self.show_routes(skip: 0, limit: 1)
     self.get("/routes", params: {skip: skip, limit: limit})
   end
 
   def self.create_catch_all
+    routes = self.show_routes(limit: 100)
+    match = []
+    routes["items"].each do |item|
+      if item["description"] == "Catch All Route - Created By Onebody" and item["expression"] == "match_recipient('.*@#{Site.current.email_host}')"
+        match << item
+      end
+    end
+    if match.empty?
+      self.post("https://api:#{APIKEY}@api.mailgun.net/v2/routes", body: self.build_data)
+    else
+      "Found #{match.length} match(es)- #{match}"
+    end
+  end
+
+  private
+
+  def self.build_data
     data = {}
     data[:priority] = 0
-    data[:description] = "Catch All Route"
+    data[:description] = "Catch All Route - Created By Onebody"
     data[:expression] = "match_recipient('.*@#{Site.current.email_host}')"
     data[:action] = ["forward('http://#{Site.current.host}/emails.mime')", "stop()"]
-    self.post("https://api:#{APIKEY}@api.mailgun.net/v2/routes", body: data)
+    return data
   end
 
 end
